3c0ef985419c2465b53554228b675dc6dc39e140
Ji-Rath <garethaguilar@gmail.com>
2022-04-12 14:09:45 -0500

    Add InteractionLibrary for implementing general static functions

---
+++ Source/InteractionSystem/InteractionLibrary.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractionLibrary.h"
+
+#include "Inventory/InventoryInfo.h"
+
+FName UInteractionLibrary::GetRowName(const FInventoryContents& Item)
+{
+	return Item.RowHandle.IsNull() ? Item.RowName : Item.RowHandle.RowName;
+}
---
+++ Source/InteractionSystem/InteractionLibrary.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Inventory/InventoryInfo.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "InteractionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERACTIONSYSTEM_API UInteractionLibrary : public UBlueprintFunctionLibrary
+{
+ GENERATED_BODY()
+public:
+ UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Inventory")
+ static FName GetRowName(const FInventoryContents& Item);
+};
